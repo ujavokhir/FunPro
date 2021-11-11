6eab06eb91d7893a32ca8fa9e88030e3f35e8ae7
Javokhir Umarjonov <91652129+ujavokhir@users.noreply.github.com>
2021-11-11 18:46:50 +0500

    Add files via upload

---
+++ Week-3/Leap Year/Leap Year/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace Leap_Year
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nudYear = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudYear)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(97, 113);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Check if the year Leap Year or not?";
+            // 
+            // nudYear
+            // 
+            this.nudYear.Location = new System.Drawing.Point(52, 66);
+            this.nudYear.Maximum = new decimal(new int[] {
+            1410065407,
+            2,
+            0,
+            0});
+            this.nudYear.Name = "nudYear";
+            this.nudYear.Size = new System.Drawing.Size(120, 20);
+            this.nudYear.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 184);
+            this.Controls.Add(this.nudYear);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCheck);
+            this.Name = "Form1";
+            this.Text = "Leap Year";
+            ((System.ComponentModel.ISupportInitialize)(this.nudYear)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudYear;
+    }
+}
+
---
+++ Week-3/Leap Year/Leap Year/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Leap_Year
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            var Year = nudYear.Value;
+            bool isLeap;
+            if (Year > 1582)
+            {
+                if (Year % 4 == 0)
+                {
+                    if(Year % 100 == 0)
+                    {
+                        if (Year % 400 == 0)
+                        {
+                            isLeap = true;
+                        }
+                        else
+                        {
+                            isLeap = false;
+                        }
+                    }
+                    else
+                    {
+                        isLeap = true;
+                    }
+                }
+                else
+                {
+                    isLeap = false;
+                }
+            }
+            else
+            {
+                isLeap = false;
+            }
+            if (isLeap)
+                MessageBox.Show("It is a leap year");
+            else
+                MessageBox.Show("It is not a leap year");
+        }
+    }
+}
